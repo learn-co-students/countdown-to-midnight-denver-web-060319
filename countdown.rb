37e181ebc2416bce891483045315d33a031ff36c
Rj <rikrdo.ortega@gmail.com>
2019-05-08 16:30:54 -0600

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,4 @@
 def countdown(sec)
-    sec = sec
     while sec > 0
         puts "#{sec} second(s)!".upcase
         sec -= 1
@@ -8,11 +7,10 @@ def countdown(sec)
 end
 
 def countdown_with_sleep(sec)
-    sec = sec
     while sec > 0
         puts "#{sec} second(s)!".upcase
         sec -= 1
         sleep 1
     end
-        "happy new year!".upcase
+    "happy new year!".upcase
 end
