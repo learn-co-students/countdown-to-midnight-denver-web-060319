d47ba89fcbdbddab30adf1efd231fe6413661c69
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-28 19:57:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
-#write your code here
+def countdown(integer)
+  integer = 10
+  
+  while integer >= 1
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
 
-def countdown
+end
+
+def countdown_with_sleep(integer)
+  integer = 10
+  
+  while integer >= 1
+    sleep(1)
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
+
 end
